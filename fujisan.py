0371c4423dc7451723983c305e2c5d6960c283c8
SVARTLAB <Svartlab@sew-retina.local>
2016-05-19 16:05:43 +0200

    redesign

--- fujisan.py
+++ fujisan.py
@@ -1,6 +1,6 @@
 # fujisan.py
 # ----------
-# FDIA clone by isak.asia, now a daemon!
+# FDIA clone by isak.asia
 #
 # dependencies: pymssql, pillow
 
@@ -8,83 +8,142 @@ import os
 import time
 import shutil
 import pymssql
+from collections import namedtuple
 from PIL import Image # Pillow library
 import fujirawimage # custom Pillow decoder
 
 # configuration
-inspool = "/tmp/fuji/inspool" # inspool mount path
-outspool = "/tmp/fuji/outspool" # outspool mount path
-
+tmp = "/tmp/fuji/" # working path for mounts, trailing slash please
 outdir = "/Users/Svartlab/Documents/Frontier/Ready" # output path
-w2khost = "10.0.1.15" # mssql / smb share host
-w2kuser = "svartlab"
-w2kpass = "1234"
+
+win2000host = "10.0.1.15" # mssql / smb share host
+
+mssqluser = "svartlab"
+mssqlpass = "1234"
 
 datestr = time.strftime("%Y%m%d") # today's date
 
+# windows to OS X path conversion
+def osxpath(win2000path):
+	return win2000path.replace("\\", "/").replace("//WIN2000/", "")
+
+# mount inspool if not mounted	
+inspool = tmp + "InSpool"
+if not os.path.ismount(inspool):
+	if not os.path.isdir(inspool):
+		os.makedirs(inspool)
+	os.system("mount_smbfs //FRONTIER:FRONTIER@" + win2000host + "/InSpool " + inspool);
+
+# mount outspool if not mounted
+outspool = tmp + "OutSpool"
+if not os.path.ismount(outspool):
+	if not os.path.isdir(outspool):
+		os.makedirs(outspool)
+	os.system("mount_smbfs //FRONTIER:FRONTIER@" + win2000host + "/OutSpool " + outspool);
+
+# create output directory if not existing
+if not os.path.isdir(outdir):
+	os.makedirs(outdir)
+
+# create working directory if not existing
+if not os.path.isdir(tmp):
+	os.makedirs(tmp)
+
+def exportimages(images, ordername, extension = ".tif", path = outdir):
+	newdir = path + "/" + datestr + "-" + ordername + "/"
+	if not os.path.isdir(newdir):
+		os.makedirs(newdir)
+
+	for image in images:
+		try:
+			im = Image.open(tmp + image.imgfile)
+		except IOError as e:
+			print "I/O error({0}): {1}".format(e.errno, e.strerror)
+
+		filename = newdir + datestr + "-" + ordername + "-" + image.id.zfill(3) + extension
+		out = im.rotate(270, expand = True)
+		print "saving -" + filename + "-"
+		out.save(filename)
+
 # connect to MS SQL database
-with pymssql.connect(w2khost, w2kuser, w2kpass, "FDIA_DB") as conn:
+with pymssql.connect(win2000host, mssqluser, mssqlpass, "FDIA_DB") as conn:
 		
 	# reset list of orders waiting to be converted
-	waiting_orders = []
+	orderclass = namedtuple("order", ["id", "name", "status", "inffile"])
+	imageclass = namedtuple("image", ["id", "imgfile", "inffile"])
+	orders = []
+
 	with conn.cursor(as_dict = True) as c:
+
 		# retrieve all orders
-		c.execute("SELECT * FROM dbo.OrderTable")
+		c.execute("SELECT FDIAManageID as id, OrderID as name, Status as status, InfFileName as inffile FROM dbo.OrderTable")
+
 		# loop through rows
 		for row in c:
-			# read order status 
+			# append to order list
+			orders.append(orderclass(str(row["id"]), row["name"], row["status"], osxpath(row["inffile"])))
+
+
+		# loop through orders
+		for order in orders:
+			print order
+			c.execute(	"SELECT	o.ImageID as id, o.FileName as imgfile, i.InfFileName as inffile "
+						"FROM dbo.OutputImageTable o, dbo.InputImageTable i "
+						"WHERE o.FDIAManageID = " + order.id + " "
+						"AND i.FDIAManageID = o.FDIAManageID "
+						"AND i.ImageID = o.ImageID" )
+
+			images = []
+			for row in c:
+				images.append(imageclass(str(row["id"]), osxpath(row["imgfile"]), osxpath(row["inffile"])))
+
+			# print images
+
 			# (0 = Error, 2 = Waiting to convert)
 			# (4 = Waiting to write, 5 = Completed)
-			if row["Status"] == 2:
-				# append to order list
-				waiting_orders.append({
-					"dir":str(row["FDIAManageID"]), 
-					"status":row["Status"], 
-					"name":row["OrderID"]
-				})
-		# mount inspool if not mounted	
-		if not os.path.ismount(inspool):
-			if not os.path.isdir(inspool):
-				os.makedirs(inspool)
-			os.system("mount_smbfs //FRONTIER:FRONTIER@" + win2000 + "/InSpool " + inspool);
+	
+			# if waiting to convert
+			if order.status == 2:
+				exportimages(images, order.name, ".tif")
 
-		# mount outspool if not mounted
-		if not os.path.ismount(outspool):
-			if not os.path.isdir(outspool):
-				os.makedirs(outspool)
-			os.system("mount_smbfs //FRONTIER:FRONTIER@" + win2000 + "/OutSpool " + outspool);
+				# change status of order to completed in DB
+				c.execute("UPDATE dbo.OrderTable SET Status = 5 WHERE FDIAManageID = '" + order.id + "'")
 
-		# create output directory if not existing
-		if not os.path.isdir(outdir):
-			os.makedirs(outdir)
+			# if completed, remove
+			elif order.status == 5:
 
-		# loop through orders to be converted
-		for order in waiting_orders:
-			orderdir = inspool + "/0000" + order["dir"]
-			newdir = outdir + "/" + datestr + "-" + order["name"]
-			if not os.path.isdir(newdir):
-				os.makedirs(newdir)
+				exportimages(images, order.name, ".tif", "/tmp/BACKUP")
 
-			for file in glob.glob(orderdir + "/*.RAW"):
-				im = Image.open(file)
-				filename = newdir + "/" + datestr + "-" + order["name"] + "-" + os.basename(file).replace(".RAW", "").replace("0000", "")
-				out = im.rotate(270, expand = True)
-				out.save(filename + ".tif")
-				print "saving " + filename
+				# find unique folders associated with current order
+				orderdirs = set()
 
-			# remove image directory
-			shutil.rmtree(orderdir)
+				# add each path name to image or .INF file, set() eliminates duplicates
+				for image in images:
+					orderdirs.add(os.path.dirname(image.imgfile))
+					orderdirs.add(os.path.dirname(image.inffile))
 
-			# remove images associated with order in DB
-			c.execute("DELETE FROM dbo.ImageTable WHERE FDIAManageID = '" + order["dir"] + "'")
-			c.execute("DELETE FROM dbo.OutputImageTable WHERE FDIAManageID = '" + order["dir"] + "'")
-			# TODO: insert more tables here
+				# loop through & remove the folders
+				for d in orderdirs:
+					print "rm -rf " + tmp + d
+					if os.path.isdir(tmp + d):
+						shutil.rmtree(tmp + d)
 
-			# change status of order to completed in DB
-			#c.execute("UPDATE dbo.OrderTable SET Status = 5 WHERE FDIAManageID = '" + order["dir"] + "'")
+				# remove images associated with order in DB
+				c.execute("DELETE FROM dbo.ImageTable WHERE FDIAManageID = '" + order.id + "'")
+				c.execute("DELETE FROM dbo.InputImageTable WHERE FDIAManageID = '" + order.id + "'")
+				c.execute("DELETE FROM dbo.OutputImageTable WHERE FDIAManageID = '" + order.id + "'")
+				c.execute("DELETE FROM dbo.OutputEzTable WHERE FDIAManageID = '" + order.id + "'")
+				c.execute("DELETE FROM dbo.OtherTable WHERE FDIAManageID = '" + order.id + "'")
+				c.execute("DELETE FROM dbo.Old_convertInformationTable WHERE FDIAManageID = '" + order.id + "'")
+				print "DELETE FROM dbo.xxxTable WHERE FDIAManageID = " + order.id
 
-			# or nah, remove it
-			c.execute("DELETE FROM dbo.OrderTable WHERE FDIAManageID = '" + order["dir"] + "'")
+				# remove order in DB
+				c.execute("DELETE FROM dbo.OrderTable WHERE FDIAManageID = '" + order.id + "'")
+
+				# remove order .INF file
+				if os.path.isfile(tmp + order.inffile):
+					print "os.remove(" + tmp + order.inffile + ")"
+					os.remove(tmp + order.inffile)
 
 			# commit SQL changes to database
 			conn.commit()
